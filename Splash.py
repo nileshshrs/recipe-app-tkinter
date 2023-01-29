06bf673e9d39890c19fa405344e03b6e2aafe670
nileshshrs <nileshshr54@gmail.com>
2023-01-29 18:29:47 +0545

    splashscreen for tkinter gui application created

--- Splash.py
+++ Splash.py
@@ -1,1 +1,26 @@
-tkinter="this is a recipe app."
+from tkinter import *
+from customtkinter import *
+import customtkinter
+from PIL import ImageTk , Image
+#from form import loginPage
+splashScreen=CTk()
+splashScreen.title("splashscreen")
+splashScreen.overrideredirect(True)
+screenWidth= splashScreen.winfo_screenwidth()
+screenHeight= splashScreen.winfo_screenheight()
+splashScreenWidth=350
+splashScreenHeight=400
+splashScreenX=(screenWidth/2)-(splashScreenWidth/2)
+splashscreenY=(screenHeight/2)-(splashScreenHeight/2)
+def killWindow():
+    splashScreen.destroy()
+   # loginPage()
+splashScreen.geometry(f"{splashScreenWidth}x{splashScreenHeight}+{int(splashScreenX)}+{int(splashscreenY)}")
+bgImage=customtkinter.CTkImage(light_image=Image.open("Assets/1.png"),size=(300, 300))
+bgLabel=customtkinter.CTkLabel(splashScreen, text="", image=bgImage)
+bgLabel.pack(fill=BOTH)
+bgLabel.pack_propagate(False)
+splashScreen.after(3000, killWindow)
+splashScreen.mainloop()
+
+
