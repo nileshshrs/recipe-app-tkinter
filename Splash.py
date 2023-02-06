722bfe95530be174b95bc9c93d64dbc859143f20
nileshshr <nileshshr54@gmail.com>
2023-02-06 09:03:23 +0545

    code made much more readable

--- Splash.py
+++ Splash.py
@@ -3,6 +3,7 @@ from customtkinter import *
 import customtkinter
 from PIL import ImageTk , Image
 from form import loginPage
+
 splashScreen=CTk()
 splashScreen.title("splashscreen")
 splashScreen.overrideredirect(True)
@@ -12,9 +13,11 @@ splashScreenWidth=350
 splashScreenHeight=400
 splashScreenX=(screenWidth/2)-(splashScreenWidth/2)
 splashscreenY=(screenHeight/2)-(splashScreenHeight/2)
+
 def killWindow():
     splashScreen.destroy()
     loginPage()
+    
 splashScreen.geometry(f"{splashScreenWidth}x{splashScreenHeight}+{int(splashScreenX)}+{int(splashscreenY)}")
 bgImage=customtkinter.CTkImage(light_image=Image.open("Assets/1.png"),size=(300, 300))
 bgLabel=customtkinter.CTkLabel(splashScreen, text="", image=bgImage)
--- form.py
+++ form.py
@@ -6,15 +6,19 @@ from tkinter import ttk
 from PIL import ImageTk, Image
 import sqlite3
 from main import mainScreen
+
 textcolor = "whitesmoke"
+
 try:
     cnxt = sqlite3.connect("data/userdata.db")
 except sqlite3.Error as e:
     print(f"Error connecting to the database: {e}")
 c = cnxt.cursor()
 c.execute("CREATE TABLE IF NOT EXISTS userdata (USERNAME TEXT PRIMARY KEY NOT NULL UNIQUE, FIRSTNAME TEXT NOT NULL, LASTNAME TEXT NOT NULL, EMAIL TEXT NOT NULL, PASSWORD TEXT NOT NULL)")
+
 def loginPage():
     global bgImage
+
     window = CTk()
     appWidth = 360
     appHeight = 470
@@ -25,18 +29,22 @@ def loginPage():
     window.geometry(f"{appWidth}x{appHeight}+{int(centerX)}+{int(centerY)}")
     window.resizable(False, False)
     window.title("login")
+
     def navigate():
         window.destroy()
         registerPage()
+
     def getUserData():
         global credentials
         username = usernameEntry.get().lower()
         password = passwordEntry.get()
+
         try:
             c.execute("select * from userdata where USERNAME='" + username+"' and PASSWORD='"+password+"'")
             userdata = c.fetchall()
         except sqlite3.Error as e:
             print(f"Error executing the database query: {e}")
+
         if userdata != []:
             for user in userdata:
                 if username and password in user:
@@ -51,43 +59,62 @@ def loginPage():
                                  bg_color="lightpink", text_color="firebrick")
             errorLabel.after(5000, lambda: errorLabel.configure(
                 text="", bg_color="#2B2B2B"))
+
     frame1 = customtkinter.CTkFrame(
         window, height=appHeight, width=appWidth, bg_color="black")
+
     frame1.pack(fill="both")
+
     frame1.pack_propagate(False)
+
     loginLabel = customtkinter.CTkLabel(master=frame1, text="Sign In !", font=(
         "helvatica", 20, "bold"), text_color=textcolor)
+
     loginLabel.place(x=26, y=20)
+
     bgImage = customtkinter.CTkImage(
         light_image=Image.open("Assets/4.png"), size=(175, 141))
+
     imgWidget = customtkinter.CTkLabel(master=frame1, image=bgImage, text="")
     imgWidget.pack(pady=55, fill="both")
+
     errorLabel = customtkinter.CTkLabel(
         master=frame1, text="", width=300, height=25)
     errorLabel.place(y=200, x=25)
+
     usernameEntry = customtkinter.CTkEntry(
         master=frame1, placeholder_text="username", width=300, height=35,)
     usernameEntry.place(y=250, x=25)
+
     passwordEntry = customtkinter.CTkEntry(
         master=frame1, placeholder_text="password", width=300, height=35)
     passwordEntry.place(y=300, x=25)
+
     passwordEntry.configure(show="*")
     loginbtn = customtkinter.CTkButton(
         master=frame1, text="Login", width=300, height=35, command=getUserData)
+
     loginbtn.place(y=350, x=25)
     registerLabel = customtkinter.CTkLabel(
         master=frame1, text="Dont have an account?", font=("helvatica", 16))
+
     registerLabel.place(y=405, x=25)
+
     registerBtn = customtkinter.CTkButton(
         master=frame1, text="Register now", width=120, command=navigate)
     registerBtn.place(x=204, y=405)
+
     window.mainloop()
 
 def registerPage():
+
     global registerImg
+
     window = CTk()
+
     registerImg = customtkinter.CTkImage(
         light_image=Image.open("Assets/5.png"), size=(250, 250))
+
     appWidth = 850
     appHeight = 500
     screenHeight = window.winfo_screenheight()
@@ -97,9 +124,11 @@ def registerPage():
     window.geometry(f"{appWidth}x{appHeight}+{int(centerX)}+{int(centerY)}")
     window.resizable(False, False)
     window.title("register")
+
     def navigate():
         window.destroy()
         loginPage()
+
     def getUserData():
         firstname = registerFirstName.get().lower()
         lastname = registerLastName.get().lower()
@@ -107,7 +136,9 @@ def registerPage():
         email = registeremail.get().lower()
         password = registerpassword.get()
         confirmpassword = registerconfirmpassword.get()
+
         c.execute("select * from userdata where USERNAME='"+username+"'")
+
         dataResult = c.fetchall()
         userData = [
             (firstname),
@@ -116,6 +147,7 @@ def registerPage():
             (email),
             (password),
         ]
+
         if dataResult == []:
             if firstname == "" or lastname == "" or username == "empty" or password == "" or email == "" or confirmpassword == "":
                 errorLabel.configure(
@@ -162,50 +194,80 @@ def registerPage():
         registeremail.delete(0, END)
         registerpassword.delete(0, END)
         registerconfirmpassword.delete(0, END)
+
     imageFrame = customtkinter.CTkFrame(
         master=window, height=appHeight, width=.35*appWidth,)
+
     imageFrame.place(x=0, y=0)
+
     imageFrame.propagate(False)
+
     imgWidget = customtkinter.CTkLabel(
         master=imageFrame, image=registerImg, height=appHeight, width=.35*appWidth, text="")
+
     imgWidget.pack(padx=20)
+
     formFrame = customtkinter.CTkFrame(
         master=window, height=appHeight, width=.65*appWidth)
+
     formFrame.place(x=296, y=0)
+
     formFrame.pack_propagate(False)
+
     formFrame.propagate(False)
+
     registerTitle = customtkinter.CTkLabel(master=formFrame, text="Sign up...!", font=(
         "helvatica", 25, "bold"), text_color=textcolor)
+
     registerTitle.place(x=50, y=25)
+
     errorLabel = customtkinter.CTkLabel(master=formFrame, text="", width=420)
+
     errorLabel.place(y=90, x=57)
+
     registerFirstName = customtkinter.CTkEntry(
         master=formFrame, width=205, placeholder_text="first name", height=30)
+
     registerFirstName.place(x=57, y=140)
+
     registerLastName = customtkinter.CTkEntry(
         master=formFrame, width=205, placeholder_text="last name", height=30)
+
     registerLastName.place(x=270, y=140)
+
     registerusername = customtkinter.CTkEntry(
         master=formFrame, width=417, placeholder_text="username", height=30)
+
     registerusername.place(x=57, y=190)
+
     registeremail = customtkinter.CTkEntry(
         master=formFrame, width=417, placeholder_text="email", height=30)
+
     registeremail.place(x=57, y=240)
+
     registerpassword = customtkinter.CTkEntry(
         master=formFrame, width=417, placeholder_text="password", height=30)
+
     registerpassword.place(x=57, y=290)
+
     registerpassword.configure(show="*")
+
     registerconfirmpassword = customtkinter.CTkEntry(
         master=formFrame, width=417, placeholder_text="confirm-password", height=30)
     registerconfirmpassword.place(x=57, y=340)
+
     registerconfirmpassword.configure(show="*")
+
     registerBtn = customtkinter.CTkButton(
         master=formFrame, text="Sign up", width=417, height=30, command=getUserData)
     registerBtn.place(x=57, y=390)
+
     loginLabel = customtkinter.CTkLabel(
         master=formFrame, text="Dont  have  an  account  ?", font=("helvatica", 16, "bold"))
     loginLabel.place(x=57, y=440)
+
     registerBtn = customtkinter.CTkButton(
         master=formFrame, text="login", width=200, command=navigate)
     registerBtn.place(x=270, y=440)
+    
     window.mainloop()
